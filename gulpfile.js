aa7971999aa5dfd97c9d4b79274ace9518cf1791
sergcms <sergey.fryntsko@gmail.com>
2017-12-26 23:51:54 +0200

    alignment of markup

--- gulpfile.js
+++ gulpfile.js
@@ -5,48 +5,48 @@ const merge = require('merge-stream');
 const tinypng = require('gulp-tinypng');
 
 gulp.task('autoprefixer', function() { // создаем таск autoprefixer 
-  gulp.src('src/css/**/*.css')       // файлы которые будем обрабатывать
+    gulp.src('src/css/**/*.css')       // файлы которые будем обрабатывать
     .pipe(autoprefixer({             // запуск autoprefixer
-      browsers: ['last 2 versions'],
-      cascade: false
+        browsers: ['last 2 versions'],
+        cascade: false
     }))
     .pipe(gulp.dest('app/css'))      // выгружаем обработанные файлы
 });
 
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('src/sprite/*.png')
-      .pipe(spritesmith({ // Настройка спрайта
-          imgName: 'sprite.png',
-          cssName: 'sprite.css',
-          imgPath: '../img/sprite.png'
-      }));
+    var spriteData = gulp.src('src/sprite/*.png')
+        .pipe(spritesmith({ // Настройка спрайта
+            imgName: 'sprite.png',
+            cssName: 'sprite.css',
+            imgPath: '../img/sprite.png'
+        }));
 
-  var imgStream = spriteData.img
-      .pipe(gulp.dest('app/img/'));
+    var imgStream = spriteData.img
+        .pipe(gulp.dest('app/img/'));
 
-  var cssStream = spriteData.css
-      .pipe(autoprefixer({
-          browsers: ['last 2 versions'],
-          cascade: false
-      }))
-      .pipe(gulp.dest('app/css/'));
-
-  return merge(imgStream, cssStream);
+    var cssStream = spriteData.css
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css/'));
+    return merge(imgStream, cssStream);
 });
 
 gulp.task('tinypng', function () {
-  gulp.src([
-      'src/img/**/*.jpg',
-      'src/img/**/*.png'
+    gulp.src([
+        'src/img/**/*.jpg',
+        'src/img/**/*.png'
   ])
-  .pipe(tinypng('V6mnBhM_EnAKN2-1deY1hm75t1wIhFzS'))
-  .pipe(gulp.dest('app/img/'));
+    .pipe(tinypng('V6mnBhM_EnAKN2-1deY1hm75t1wIhFzS'))
+    .pipe(gulp.dest('app/img/'));
 });
 
+// gulp.task('watch', ['autoprefixer', 'sprite', 'tinypng'], function() {   // создаем таск watch
 gulp.task('watch', ['autoprefixer'], function() {   // создаем таск watch
-  gulp.watch('src/css/**/*.css', ['autoprefixer']); // следим за изменениями в файлах и обрабатуем их 
-  // gulp.watch( 'src/sprite/*.png', ['sprite'] );
-  // gulp.watch( 'src/img/**/*', ['tinypng'] );
+    gulp.watch('src/css/**/*.css', ['autoprefixer']); // следим за изменениями в файлах и обрабатуем их 
+    // gulp.watch( 'src/sprite/*.png', ['sprite'] );
+    // gulp.watch( 'src/img/**/*', ['tinypng'] );
 });
 
 gulp.task('default', ['watch']);
