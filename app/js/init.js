9743f604a892ea80d192778a79cbbe60162cc7d0
sergcms <sergey.fryntsko@gmail.com>
2018-01-07 22:48:56 +0200

    fixed init.js

--- app/js/init.js
+++ app/js/init.js
@@ -2,9 +2,13 @@ $(function ()  {
   
   // Init grid cards (all-items)
   $('#grid-thumbs').on('click', function() {
+    $('.change-icon').removeClass('active');
+    $('#grid-thumbs').addClass('active');
     $('.card.medium-card').removeClass('row-card');
   });
   $('#grid-list').on('click', function() {
+    $('.change-icon').removeClass('active');
+    $('#grid-list').addClass('active');
     $('.card.medium-card').addClass('row-card');  
   });
 
