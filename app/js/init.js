c98632f3c1a6dce00e3a3c4feba7ed86d32d1e73
sergcms <sergey.fryntsko@gmail.com>
2018-01-07 22:36:51 +0200

    fixed code

--- app/js/init.js
+++ app/js/init.js
@@ -2,14 +2,18 @@ $(function ()  {
   
   // Init grid cards
   $('#grid-thumbs').on('click', function() {
+    $('.change-icon').removeClass('active');
+    $('#grid-thumbs').addClass('active');
     $('.card.medium-card').removeClass('row-card');
   });
   $('#grid-list').on('click', function() {
+    $('.change-icon').removeClass('active');
+    $('#grid-list').addClass('active');
     $('.card.medium-card').addClass('row-card');  
   });
   
   // Init slick slider
-  $('.').slick({
+  $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
