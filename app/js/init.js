3603481b5d6275195f9688dbfa72f905fb26baad
sergcms <sergey.fryntsko@gmail.com>
2018-01-14 22:26:51 +0200

    responsive home

--- app/js/init.js
+++ app/js/init.js
@@ -18,14 +18,42 @@ $('#grid-list').on('click', function() {
   $('.card.medium-card').addClass('row-card');  
 });
 
-// Init slick slider
+// Init slick slider with responsive
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 4,
   slidesToScroll: 4,
   autoplay: true,
   arrows: false,
-  dots: true
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 //Init form stayler for select
@@ -35,7 +63,12 @@ $('select').styler();
 let openModalsBtns = $('[data-modal]');
 openModalsBtns.on('click', function(){
   let target = $(this).attr('data-target');
-  $(target).bPopup({ });
+  $(target).bPopup({ 
+    speed: 1000,
+    transition: 'slideDown',
+    transitionClose: 'slideIn',
+    closeClass:'close-modal'
+  });
 });
 
 // Init mobile slicknav
